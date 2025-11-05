c70895aeb14413be3f0be15c4fd406b0653cdea8
Durjoy Chakraborty <durjoyckb@gmail.com>
2025-11-05 20:18:31 +0600

    Day 02 done

---
+++ Day 02/size.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+int main() {
+    int a = 18;
+    float b = 55.00;
+    char grade = 'C';
+    bool isAdult = true;
+    cout<<"Size of int = "<< sizeof(int)<<endl;
+    cout<<"Size of float = "<< sizeof(float)<<endl;
+    cout<<"Size of char = "<< sizeof(char)<<endl;
+    cout<<"Size of boolean = "<< sizeof(bool)<<endl;
+    
+    
+    return 0;
+}
