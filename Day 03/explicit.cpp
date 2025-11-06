5fe4ea057e7c621bc4de0a53baa24952d485d954
Durjoy Chakraborty <durjoyckb@gmail.com>
2025-11-06 13:31:35 +0600

    Day 03 done

---
+++ Day 03/explicit.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+using namespace std;
+int main() {
+    cout<< ((float)10/3) << endl;
+    cout<< (char)('C' + 1)<< endl;
+    return 0;
+}
---
+++ Day 03/implicit.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+using namespace std;
+int main() {
+    cout<< (10/3.0) << endl;
+    cout<< ('B' + 1)<< endl;
+    return 0;
+}
