9ab81e2ae697655172e798832c7648b7db5cacca
Durjoy Chakraborty <durjoyckb@gmail.com>
2025-11-09 13:13:50 +0600

    Day 05 done

---
+++ Day 05/N_num.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+int main() {
+    int i,num;
+    cout<<"Enter the last number: ";
+    cin>>num;
+    for(i= 1;i<=num;i++){
+        cout<< i <<" ";
+    }
+    cout<<endl;
+    return 0;
+}
