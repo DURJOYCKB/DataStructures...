aed5f084437dd2f6aa115e8363d2c26a5291e837
Durjoy Chakraborty <durjoyckb@gmail.com>
2025-11-15 20:17:28 +0600

    Day 10 done

---
+++ Day 10/outArray.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int marks[5] = {1, 2, 3, 4, 5};
+    int len  = sizeof(marks) / sizeof(int);
+
+    for(int i = 0; i<=len-1; i++){
+        cout << marks[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
