597908d069d933164b64d8ea3fc4259f5653ee8a
Durjoy Chakraborty <durjoyckb@gmail.com>
2025-11-19 12:25:52 +0600

    Day 10 done

---
+++ Day 10/reverseArray.cpp
@@ -0,0 +1,32 @@
+//Reverse An Array without Extra Space....
+
+#include<iostream>
+
+using namespace std;
+
+void Array(int *arr, int n){
+    for(int i=0;i<n;i++){
+        cout<< arr[i]<< ",";
+    }
+    cout<< endl;
+}
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int n  = sizeof(arr) / sizeof(int);
+
+    int start = 0, end = n-1;
+    while(start< end){
+        //swap..
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+
+    Array(arr, n);
+    
+    cout << endl;
+
+    return 0;
+}
