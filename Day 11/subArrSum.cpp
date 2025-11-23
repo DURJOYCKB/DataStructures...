cccb8743616b441a2ae579764e72ba32a9b4db58
Durjoy Chakraborty <durjoyckb@gmail.com>
2025-11-23 21:00:52 +0600

    Day 11 done

---
+++ Day 11/subArrSum.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void SubArr(int *arr, int n){
+    for(int start = 0;start< n; start++){
+        for(int end= start; end< n; end++){
+            int sum = 0;
+            for(int i = start; i<=end; i++){
+                sum = sum + arr[i];
+                
+            }
+            cout << sum << " ,";
+        }
+        cout << endl;
+    }
+}
+int main(){
+    int arr[6] = {2, -3, 6, -5, 4, 2};
+    int n =sizeof(arr)/sizeof(int);
+
+    SubArr(arr, n);
+    return 0;
+}
