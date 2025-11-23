1175ab08b573f5bc259bea3e814393cd0e6329c6
Durjoy Chakraborty <durjoyckb@gmail.com>
2025-11-23 21:19:40 +0600

    Day 11 done

---
+++ Day 11/maxSubarrSum.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+void SubArr(int *arr, int n){
+    int maxsum= INT32_MIN;
+    for(int start = 0;start< n; start++){
+        for(int end= start; end< n; end++){
+            int sum = 0;
+            for(int i = start; i<=end; i++){
+                sum = sum + arr[i];
+                
+            }
+            cout << sum << " ,";
+            maxsum = max(maxsum, sum);
+        }
+        cout << endl;
+    }
+    cout<< "Max SubArray Sum = "<< maxsum << endl;
+}
+int main(){
+    int arr[6] = {2, -3, 6, -5, 4, 2};
+    int n =sizeof(arr)/sizeof(int);
+
+    SubArr(arr, n);
+    return 0;
+}
