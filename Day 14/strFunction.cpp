76c9e50f2d6cffc741800ffb29d6760113d5b91f
Durjoy Chakraborty <durjoyckb@gmail.com>
2025-12-03 23:42:03 +0600

    Day 14 done

---
+++ Day 14/strFunction.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+    string str = "I love codding in c++ and java.";
+
+    cout<< str.find("c++") << endl;
+    return 0;
+
+}
