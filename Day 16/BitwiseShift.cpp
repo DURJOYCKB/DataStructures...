61b2491962bc809a3e62ece74f5a43edbf8b13b0
Durjoy Chakraborty <durjoyckb@gmail.com>
2025-12-06 20:55:45 +0600

    Day 16 done

---
+++ Day 16/BitwiseShift.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    //Bitwise  Shift Operator...
+    cout << (7 << 2) << endl;    //Left shift...
+    cout << (7 >> 2) << endl;    // Right shift...
+   
+    return 0;
+}
