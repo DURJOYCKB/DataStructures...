462f7bf7b907e096652d7f530841cda5f41eb681
Durjoy Chakraborty <durjoyckb@gmail.com>
2025-12-06 19:51:04 +0600

    Day 16 done

---
+++ Day 16/bitwiseOp.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    //Bitwise Operator...
+    cout << (3 & 5) << endl;
+    cout << (3 | 5) << endl;
+    cout << (3 ^ 5) << endl;
+    return 0;
+}
