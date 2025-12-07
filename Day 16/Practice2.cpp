3e5d7d0d4afa6cb11e1be4ba1ab2460088c46bf9
Durjoy Chakraborty <durjoyckb@gmail.com>
2025-12-07 21:55:01 +0600

    Day 16 done

---
+++ Day 16/Practice2.cpp
@@ -0,0 +1,26 @@
+
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+void clearBitsInRange(int num, int i, int j) {
+    int a = (~0) << (j+1);
+    
+    int b = (1 << i) - 1;
+    
+    int mask = a | b;
+
+    num = num & mask;
+    
+    cout << num << endl;
+}
+    
+
+int main (){
+    
+    clearBitsInRange(15, 1, 3);
+
+    
+    return 0;
+}
