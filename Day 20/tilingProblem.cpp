14c29d716a0d14dde9ad4a72b890736583ea256d
Durjoy Chakraborty <durjoyckb@gmail.com>
2025-12-11 19:23:37 +0600

    Day 20 done

---
+++ Day 20/tilingProblem.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int tilingProblem(int n) {
+    if (n == 0 || n == 1){
+        return 1;
+    }
+
+    return tilingProblem(n-1) + tilingProblem(n-2);
+}
+
+int main (){
+    cout << tilingProblem(4) << endl;
+    cout << tilingProblem(5) << endl;
+    cout << tilingProblem(6) << endl;
+    return 0;
+}
