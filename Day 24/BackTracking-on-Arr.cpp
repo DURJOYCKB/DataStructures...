269cf7f9237591864f0d89fcecd561de291d74aa
Durjoy Chakraborty <durjoyckb@gmail.com>
2025-12-19 12:22:17 +0600

    Day 24 done

---
+++ Day 24/BackTracking-on-Arr.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void printArr(int arr[], int n){
+    for(int i= 0; i<n; i++){
+        cout << arr[i] <<" ";
+    }
+    cout << endl;
+}
+
+void ChangeArr(int arr[], int n, int i){
+    if(i == n){
+        printArr(arr, n);
+        return ;
+    }
+    arr[i] = i+1;
+    ChangeArr(arr, n, i+1);
+    arr[i] -= 2;             //Backtracking...
+
+}
+
+int main(){
+    int arr[5] = {0};
+    int n = 5;
+    ChangeArr(arr, n, 0);
+    printArr(arr, n);
+    return 0;
+}
