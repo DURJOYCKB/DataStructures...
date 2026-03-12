fa6db4868577fe2674b1a635ac0ef83278fc5a3b
Durjoy Chakraborty <durjoyckb@gmail.com>
2026-03-12 21:26:44 +0600

    Day 28 half

---
+++ Day 28/Dupli-parenthesis.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+
+bool isDuplicate(string str){
+    stack<char> s;
+
+    for(int i= 0; i<str.size(); i++){
+        int ch = str[i];
+
+        if(ch != ')'){
+            s.push(ch);
+        } else {
+            if(s.top() == '('){
+                return true;
+            }
+
+            while(s.top() != '('){
+                s.pop();
+            }
+            s.pop();
+        }
+    }
+    return false;
+}
+
+int main(){
+
+    string str1 = "((a+b))";        //invalid: true
+    string str2 = "((a+b)+(c+d))";  //valid: false
+
+    cout << isDuplicate(str1) << endl;
+    cout << isDuplicate(str2) << endl;
+
+    return 0;
+}
